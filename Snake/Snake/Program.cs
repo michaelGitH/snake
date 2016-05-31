15d940a0c003563d163f79609e2b419cb5696fce
mihail <tarakanov.m.a@mail.ru>
2016-06-01 00:30:19 +0300

    Legasy and class Figure

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,17 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            HorizontalLine hLine = new HorizontalLine(5, 10, 8, '+');
-            hLine.Drow();
 
-            VerticalLine vLine = new VerticalLine(6, 11, 7, '+');
-            vLine.Drow();
 
             Console.ReadLine();
         }
