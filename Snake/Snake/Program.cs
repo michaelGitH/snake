f848baf4ebfd4230619ab29907c0fd7c7a84524a
mihail <tarakanov.m.a@mail.ru>
2016-06-01 00:12:11 +0300

    created new classes: HL and VL

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,62 +16,11 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(8, 8, '$');
-            p3.Draw();
+            HorizontalLine hLine = new HorizontalLine(5, 10, 8, '+');
+            hLine.Drow();
 
-            Point p4 = new Point(8, 15, '$');
-            p4.Draw();
-
-            Point p5 = new Point(20, 15, '$');
-            p5.Draw();
-
-            Point p6 = new Point(20, 8, '$');
-            p6.Draw();
-
-            List < int > numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[ 0 ];
-            int y = numList[ 1 ];
-            int z = numList[ 2 ];
-
-            foreach( int i in numList )
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> plist = new List<Point>();
-            plist.Add(p1);
-            plist.Add(p2);
-
-            List<char> symb = new List<char>();
-            symb.Add('!');
-            symb.Add('@');
-            symb.Add('#');
-            symb.Add('$');
-            symb.Add('*');
-
-            foreach(char i in symb)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> rec = new List<Point>();
-            rec.Add(p3);
-            rec.Add(p4);
-            rec.Add(p5);
-            rec.Add(p6);
-
-
-            for ( int i = 0; i < 4; i++ )
-            {
-                Console.WriteLine(rec[ i ]);
-            }
-
+            VerticalLine vLine = new VerticalLine(6, 11, 7, '+');
+            vLine.Drow();
 
             Console.ReadLine();
         }
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+        public VerticalLine(int yHight, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yHight; y < yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+               p.Draw();
+            }
+        }
+    }
+}
